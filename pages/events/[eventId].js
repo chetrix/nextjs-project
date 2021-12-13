651ae93bb40fbdec511b73735324218c0cee25a1
Amit Chetri <amit.chhetri95@gmail.com>
2021-12-13 14:23:55 +0530

    Event detail page 76

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,32 @@
-export default function EventDetailPage(){
-    return(
-        <div>
-            Event Detail
-        </div>
-    )
+import { useRouter } from "next/router";
+import { Fragment } from "react/cjs/react.production.min";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
