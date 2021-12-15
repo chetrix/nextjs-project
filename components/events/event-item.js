df6551dbe85af7dd4c8fc5276f86b8fb33c8ea11
Amit Chetri <amit.chhetri95@gmail.com>
2021-12-16 00:23:48 +0530

    Image optimization

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import Button from "../ui/button";
 import classes from "./event-item.module.css";
 import DateIcon from "../icons/date-icon";
@@ -18,7 +20,7 @@ export default function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={340} height={160}/>
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
